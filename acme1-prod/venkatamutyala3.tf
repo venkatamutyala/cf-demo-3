e00def6823b799773351d2aa08df970cb0c3b826
Venkat <venkata@venkatamutyala.com>
2024-08-28 16:28:32 +0000

    feat: adding waf

--- acme1-prod/venkatamutyala3.tf
+++ acme1-prod/venkatamutyala3.tf
@@ -8,3 +8,20 @@ module "venkatamutyala3_cert" {
 output "venkatamutyala3_dns_options" {
   value = module.venkatamutyala3_cert.domain_validation_options
 }
+
+
+module "venkatamutyala3_cloudfront" {
+  source             = "../modules/cloudfront"
+  origin_domain_name = local.origin_domain_name
+  web_acl_id         = local.web_acl_id
+  tags = {
+    "captain_domain"           = local.captain_domain,
+    "kubernetes_resource_name" = "venkatamutyala3_cloudfront"
+  }
+  aliases            = module.venkatamutyala3_cert.aliases
+  acm_certificate_arn = module.venkatamutyala3_cert.certificate_arn
+}
+
+output "venkatamutyala3_cdn" {
+  value = module.venkatamutyala3_cloudfront.distributions
+}
