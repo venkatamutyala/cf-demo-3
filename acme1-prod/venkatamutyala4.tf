7da51f5573d2b180da2081761d35aa9670185232
Venkat <venkata@venkatamutyala.com>
2024-08-29 17:09:52 +0000

    feat: adding CDN for venkatamutyala4

--- acme1-prod/venkatamutyala4.tf
+++ acme1-prod/venkatamutyala4.tf
@@ -8,3 +8,20 @@ module "venkatamutyala4_cert" {
 output "venkatamutyala4_dns_options" {
   value = module.venkatamutyala4_cert.domain_validation_options
 }
+
+
+module "venkatamutyala4_cloudfront" {
+  source             = "../modules/cloudfront"
+  origin_domain_name = local.origin_domain_name
+  web_acl_id         = local.web_acl_id
+  tags = {
+    "captain_domain"           = local.captain_domain,
+    "kubernetes_resource_name" = "venkatamutyala4_cloudfront"
+  }
+  aliases            = module.venkatamutyala4_cert.aliases
+  acm_certificate_arn = module.venkatamutyala4_cert.certificate_arn
+}
+
+output "venkatamutyala4_cdn" {
+  value = module.venkatamutyala4_cloudfront.distributions
+}
